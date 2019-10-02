d1deb691f6474a99b3ca39935525aef126d828c5
Nat Gordon <nat@ionata.com.au>
2019-10-02 16:03:04 +1000

    Add missing docstrings

--- src/webapp/apps.py
+++ src/webapp/apps.py
@@ -1,9 +1,13 @@
+"""Config for the webapp app."""
 from django.apps import AppConfig
 
 
 class WebAppConfig(AppConfig):
+    """Config for the webapp app."""
+
     name = "webapp"
 
     def ready(self):
+        """Import signals."""
         # noqa pylint: disable=unused-import
         from webapp import signals
